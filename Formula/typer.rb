b613fb38807fc3f1a41795908d7a47e0cda34093
William Gerstung <willgerstung@gmail.com>
2026-02-11 00:23:42 -0600

    bump typer to v0.3.1

--- Formula/typer.rb
+++ Formula/typer.rb
@@ -3,8 +3,8 @@ class Typer < Formula
 
   desc "Typing practice in your terminal — like monkeytype for the CLI"
   homepage "https://github.com/William-Ger/typer"
-  url "https://github.com/William-Ger/typer/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "2ce15b1e3d23ad0da9a0a5275b9120bb918346b93d3cbce35692a1ac1cad7341"
+  url "https://github.com/William-Ger/typer/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "0cfe8eb55b3bdc8f4b5c1c8437e8cb66bf37602ae7e055b0a50cf98bc1e54016"
   license "MIT"
 
   depends_on "python@3.12"
